9cb47d9885b07d2310e516e6dc3db61474b110c5
Raian010 <md.abrarulhoque123@gmail.com>
2023-10-19 16:02:24 +0600

    Added one database for my cart section

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require("dotenv").config();
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -38,13 +38,13 @@ async function run() {
       res.send(result);
     })
 
-    app.get("/cars/:id", async(req,res) => {
-      const id = req.params.id;
-      console.log(id);
-      const query = { _id: new ObjectId(id)};
-      const result = await carsCollection.findOne(query);
-      res.send(result)
-    })
+    // app.get("/cars/:id", async(req,res) => {
+    //   const id = req.params.id;
+    //   console.log(id);
+    //   const query = { _id: new ObjectId(id)};
+    //   const result = await carsCollection.findOne(query);
+    //   res.send(result)
+    // })
 
     app.post("/cars", async(req,res) => {
       const user = req.body;
@@ -52,6 +52,7 @@ async function run() {
       const result = await carsCollection.insertOne(user);
       res.send(result);
     })
+
 
 
     // Carts collection
@@ -64,6 +65,13 @@ async function run() {
     app.post("/carts", async(req,res) => {
       const user = req.body;
       const result = await cartsCollection.insertOne(user);
+      res.send(result);
+    })
+
+    app.delete("/carts/:id", async(req,res) => {
+      const id = req.params.id;
+      const query = { _id : new ObjectId(id)};
+      const result = await cartsCollection.deleteOne(query);
       res.send(result);
     })
 
